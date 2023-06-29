96b72ba2a2bcddd2dbb818e8b35d8a4f68e75a28
Cloudguy-IT <cloudguy.it@gmail.com>
2023-06-29 17:07:09 -0600

    Testing trivy report 25

--- .github/workflows/json2markdown.py
+++ .github/workflows/json2markdown.py
@@ -21,12 +21,22 @@ def extract_vulnerabilities(data):
             vulnerabilities.append(extracted_vulnerability)
     return vulnerabilities
 
-def vulnerabilities_to_markdown(vulnerabilities):
-    markdown = "| Vulnerability ID | Package Name | Installed Version | Fixed Version | Title | Severity |\n"
-    markdown += "|------------------|--------------|-------------------|---------------|-------|----------|\n"
-    for vulnerability in vulnerabilities:
-        markdown += f"| {vulnerability['VulnerabilityID']} | {vulnerability['PkgName']} | {vulnerability['InstalledVersion']} | {vulnerability['FixedVersion']} | {vulnerability['Title']} | {vulnerability['Severity']} |\n"
-    return markdown.strip()
+def extract_coverage(data):
+    vulnerabilities = []
+    results = data.get("Results", [])
+    for result in results:
+        result_vulnerabilities = result.get("Vulnerabilities", [])
+        for vulnerability in result_vulnerabilities:
+            extracted_vulnerability = {
+                "VulnerabilityID": vulnerability.get("VulnerabilityID"),
+                "PkgName": vulnerability.get("PkgName"),
+                "InstalledVersion": vulnerability.get("InstalledVersion"),
+                "FixedVersion": vulnerability.get("FixedVersion"),
+                "Title": vulnerability.get("Title"),
+                "Severity": vulnerability.get("Severity")
+            }
+            vulnerabilities.append(extracted_vulnerability)
+    return vulnerabilities
 
 def convert_json_to_markdown(vulnerabilities):
     # Extract headers from the keys of the first item
@@ -48,26 +58,11 @@ json_data = load_json_from_file("report.json")
 # Extract vulnerabilities
 vulnerabilities = extract_vulnerabilities(json_data)
 
-print(vulnerabilities)
-
-print("ppppppppppppppppp")
-
 with open("report_compact.json", "w") as outfile:
     json.dump(vulnerabilities, outfile)
 
 # Convert vulnerabilities to Markdown table
-markdownold = vulnerabilities_to_markdown(vulnerabilities)
 markdown = convert_json_to_markdown(vulnerabilities)
-
-print(markdownold)
-
-print("pppppppppppppp")
-
-print(markdown)
-
-# Write the Markdown table to file
-with open("reportold.md", "w") as file:
-    file.write(markdownold)
 
 # Write the Markdown table to file
 with open("report.md", "w") as file:
