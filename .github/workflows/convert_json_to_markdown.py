1e6fde685d08c28285d810b2d1de63c7558e44d3
Cloudguy-IT <cloudguy.it@gmail.com>
2023-06-29 10:32:09 -0600

    Testing trivy report 7

--- .github/workflows/convert_json_to_markdown.py
+++ .github/workflows/convert_json_to_markdown.py
@@ -1,13 +1,8 @@
-import json
-from json2markdown import json2markdown
+#importing pandas package
+import pandas as pd
 
-json_file_path = 'report.json'
-markdown_file_path = 'report.md'
+# reading JSON file
+df = pd.read_json('report.json')
 
-with open(json_file_path) as json_file:
-    json_data = json.load(json_file)
-
-markdown_content = json2markdown(json_data)
-
-with open(markdown_file_path, 'w') as markdown_file:
-    markdown_file.write(markdown_content)
+# displaying sample output
+df.sample(5)
