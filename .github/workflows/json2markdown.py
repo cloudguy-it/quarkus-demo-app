357d0face561c4f263fce17a5d1302b775a10441
cloudguy-it <94877999+cloudguy-it@users.noreply.github.com>
2023-07-18 18:44:06 -0600

    Add unittest report test (#29)
    * Add unittest report
    
    * Fix for python
    
    * Fix for python 2
    
    * Fix for python 3
    
    * Fix 1
    
    * Fix 2
    
    * Fix 3
    
    * Fix 4
    
    * Fix status
    
    * Fix status 2
    
    * Fix status 3
    
    * Fix status 4
    
    * Fix status 5
    
    * Multiples fixes
    
    * Multiples fixes 1
    
    * Multiples fixes 2
    
    * Multiples fixes 3
    
    * Multiples fixes 4
    
    * Multiples fixes 5
    
    * Multiples fixes 6
    
    * Multiples fixes 7
    
    * Multiples fixes 8
    
    * Multiples fixes 9
    
    * Multiples fixes 10
    
    * Multiples fixes 11
    
    * Multiples fixes 12
    
    * Multiples fixes 13

--- .github/workflows/json2markdown.py
+++ .github/workflows/json2markdown.py
@@ -1,8 +1,41 @@
 import json
+import sys
+import os
+from bs4 import BeautifulSoup
+
+
+def load_html_from_file(filename):
+    unittests = []
+    with open(filename) as file:
+        soup = BeautifulSoup(file, 'html.parser')
+
+    # Extract values from HTML using CSS selectors
+    tests = soup.select('#tests .counter')[0].text
+    failures = soup.select('#failures .counter')[0].text
+    ignored = soup.select('#ignored .counter')[0].text
+    duration = soup.select('#duration .counter')[0].text
+    successRate = soup.select('#successRate .percent')[0].text
+    if ( int(failures) > 0 ):
+        icon = ":x:"
+    else:
+        icon = ":white_check_mark:"
+    # Create a dictionary with extracted values
+    extracted_result = {
+        "Tests": int(tests),
+        "Failures": int(failures),
+        "Ignored": int(ignored),
+        "Duration": duration,
+        "SuccessRate": successRate,
+        "Status": icon
+    }
+    unittests.append(extracted_result)
+    return unittests
+
 
 def load_json_from_file(filename):
     with open(filename) as file:
         return json.load(file)
+
 
 def extract_vulnerabilities(data):
     vulnerabilities = []
@@ -20,48 +53,36 @@ def extract_vulnerabilities(data):
             vulnerabilities.append(extracted_vulnerability)
     return vulnerabilities
 
-def extract_coverage(data):
-    vulnerabilities = []
-    results = data.get("Results", [])
-    for result in results:
-        result_vulnerabilities = result.get("Vulnerabilities", [])
-        for vulnerability in result_vulnerabilities:
-            extracted_vulnerability = {
-                "VulnerabilityID": vulnerability.get("VulnerabilityID"),
-                "PkgName": vulnerability.get("PkgName"),
-                "InstalledVersion": vulnerability.get("InstalledVersion"),
-                "FixedVersion": vulnerability.get("FixedVersion"),
-                "Severity": vulnerability.get("Severity")
-            }
-            vulnerabilities.append(extracted_vulnerability)
-    return vulnerabilities
 
-def convert_json_to_markdown(vulnerabilities):
+def convert_json_to_markdown(formatted_json):
     # Extract headers from the keys of the first item
-    headers = list(vulnerabilities[0].keys())
+    headers = list(formatted_json[0].keys())
+
     # Generate the markdown table header
     markdown = "| " + " | ".join(headers) + " |\n"
     markdown += "| " + " | ".join(["-" * len(header) for header in headers]) + " |\n"
 
     # Generate the markdown table rows
-    for item in vulnerabilities:
+    for item in formatted_json:
         row = "| " + " | ".join(str(value) for value in item.values()) + " |\n"
         markdown += row
-
     return markdown
 
-# Load JSON data from file
-json_data = load_json_from_file("report.json")
 
-# Extract vulnerabilities
-vulnerabilities = extract_vulnerabilities(json_data)
+# Getting type of report
+type = str(sys.argv[1])
+output_file = 'report_' + type + '.md'
 
-with open("report_compact.json", "w") as outfile:
-    json.dump(vulnerabilities, outfile)
+# Load JSON
+if type == 'UNITTEST':
+    json_formatted = load_html_from_file("./build/reports/tests/test/index.html")
+elif type == 'VULNERABILITY':
+    json_data = load_json_from_file("report.json")
+    json_formatted = extract_vulnerabilities(json_data)
 
-# Convert vulnerabilities to Markdown table
-markdown = convert_json_to_markdown(vulnerabilities)
+# Convert json data to Markdown table
+markdown = convert_json_to_markdown(json_formatted)
 
 # Write the Markdown table to file
-with open("report.md", "w") as file:
+with open(output_file, "w") as file:
     file.write(markdown)
